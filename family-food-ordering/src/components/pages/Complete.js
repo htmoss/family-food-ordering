86e3088911332aea53a207effd4872d8795f6b5a
Henry Moss <htmoss7@gmail.com>
2020-07-18 21:13:34 -0700

    updated home and completed pages to fade in

--- family-food-ordering/src/components/pages/Complete.js
+++ family-food-ordering/src/components/pages/Complete.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Complete = () => {
 	return (
-		<div className='completed'>
+		<motion.div
+			className='completed'
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1 }}
+		>
 			<h1>Thank You!</h1>
 			<h4>We will get started on your order straight away.</h4>
 			<Link to='/'>
 				<button className='btn'>Start Over</button>
 			</Link>
-		</div>
+		</motion.div>
 	);
 };
 
--- family-food-ordering/src/components/pages/Home.js
+++ family-food-ordering/src/components/pages/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
 	return (
-		<div className='home'>
+		<motion.div
+			className='home'
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1 }}
+		>
 			<h1>Welcome</h1>
 			<Link to='/users'>
 				<button className='btn'>Begin</button>
 			</Link>
-		</div>
+		</motion.div>
 	);
 };
 
