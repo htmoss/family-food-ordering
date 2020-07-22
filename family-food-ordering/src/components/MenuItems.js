0df47d1799afdb4dc6f046e9d06a7325f91eada4
Henry Moss <htmoss7@gmail.com>
2020-07-22 14:41:11 -0700

    changed menu item names

--- family-food-ordering/src/components/MenuItems.js
+++ family-food-ordering/src/components/MenuItems.js
@@ -11,8 +11,8 @@ const MenuItems = () => {
 			<ul className='menu-items'>
 				<MenuItem name='eggs' price={1.99} img='eggs' />
 				<MenuItem name='bacon' price={2.99} img='bacon' />
-				<MenuItem name='stack of pancakes' price={3.99} img='pancakes' />
-				<MenuItem name='stack of waffles' price={3.99} img='waffles' />
+				<MenuItem name='pancakes' price={3.99} img='pancakes' />
+				<MenuItem name='waffles' price={3.99} img='waffles' />
 				<MenuItem name='coffee' price={1.99} img='coffee' />
 				<MenuItem name='tea' price={1.99} img='tea' />
 				<MenuItem name='cereal' price={1.99} img='cereal' />
